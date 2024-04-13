84fe911c391eddb824603d056ead2e2fd3f7a6f4
Eriks <este0023@student.monash.edu>
2024-04-13 19:53:39 +1000

    Edited images to show a carousel if multiple images are present for an item.

--- templates/Products/view.php
+++ templates/Products/view.php
@@ -15,16 +15,41 @@
         </aside>
     <?php endif; ?>
 
+
+
     <div class="d-flex align-items-center">
+        <!-- If there are images, display them -->
         <?php if (!empty($product->images)) : ?>
-            <?php foreach ($product->images as $image) : ?>
-                <img class="flex-sm-shrink-0 img-fluid rounded" src="<?= $image->file_location ?>" alt="">
-                <?php if($this->Identity->get('type') === "emp") : ?>
-                    <p>
-                        <?= $this->Html->link(__('Edit Image'), ['controller' => 'Images', 'action' => 'edit', $image->id], ['class' => 'btn btn-primary py-sm-2 px-sm-5 me-1']) ?>
-                    </p>
-                <?php endif; ?>
-            <?php endforeach; ?>
+            <!-- If there are more than one, make it a carousel -->
+            <?php if (sizeof($product->images) > 1) : ?>
+                <div id="carouselWithIndicators" class="carousel slide">
+                    <div class="carousel-inner">
+                        <?php foreach ($product->images as $image) : ?>
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="<?= $image->file_location ?>" alt="">
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselWithIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselWithIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="false"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            <!-- Else just display the one image -->
+            <?php else : ?>
+                <?php foreach ($product->images as $image) : ?>
+                    <img class="flex-sm-shrink-0 img-fluid rounded" src="<?= $image->file_location ?>" alt="">
+                    <?php if($this->Identity->get('type') === "emp") : ?>
+                        <p>
+                            <?= $this->Html->link(__('Edit Image'), ['controller' => 'Images', 'action' => 'edit', $image->id], ['class' => 'btn btn-primary py-sm-2 px-sm-5 me-1']) ?>
+                        </p>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
         <?php endif; ?>
         <div class="w-100 d-flex flex-column text-start ps-4">
             <h5 class="d-flex justify-content-between border-bottom pb-2">
