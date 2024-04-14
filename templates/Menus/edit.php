7b50a863dde875bb8c3ea58475dfd057b4eab743
Eriks <este0023@student.monash.edu>
2024-04-14 11:51:56 +1000

    Removed extra buttons from menu edit page

--- templates/Menus/edit.php
+++ templates/Menus/edit.php
@@ -9,17 +9,6 @@
     <p> You do not have permission to view this resource. </p>
 <?php else : ?>
     <div class="row">
-        <aside class="column">
-            <div class="side-nav">
-                <h4 class="heading"><?= __('Actions') ?></h4>
-                <?= $this->Form->postLink(
-                    __('Delete'),
-                    ['action' => 'delete', $menu->id],
-                    ['confirm' => __('Are you sure you want to delete # {0}?', $menu->id), 'class' => 'side-nav-item']
-                ) ?>
-                <?= $this->Html->link(__('List Menus'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            </div>
-        </aside>
         <div class="column column-80">
             <div class="menus form content">
                 <?= $this->Form->create($menu) ?>
