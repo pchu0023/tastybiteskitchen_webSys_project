3f78b1df2829a51d8c76eda8ac599fc476154363
Phuong Anh Chu (pchu0023) <pchu0023@student.monash.edu>
2024-04-15 12:37:43 +1000

    Link item with its details by click on name

--- templates/Products/index.php
+++ templates/Products/index.php
@@ -1,37 +1,45 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var iterable<\App\Model\Entity\Product> $products
  */
 ?>
- <div class="container-xxl py-5 bg-dark hero-header mb-5">
-        <div class="container text-center my-5 pt-5 pb-4">
-            <h1 class="display-3 text-white mb-3 animated slideInDown">Menu Item</h1>
-        </div>
+<div class="container-xxl py-5 bg-dark hero-header mb-5">
+    <div class="container text-center my-5 pt-5 pb-4">
+        <h1 class="display-3 text-white mb-3 animated slideInDown">Menu Item</h1>
     </div>
+</div>
 <div class="products index content">
     <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h5 class="section-title ff-secondary text-center text-primary fw-normal">Food Menu</h5>
-                    <h1 class="mb-5">Most Popular Items</h1>
-                </div>
+        <h5 class="section-title ff-secondary text-center text-primary fw-normal">Food Menu</h5>
+        <h1 class="mb-5">Most Popular Items</h1>
+    </div>
 
     <div class="row">
-        <?php foreach ($products as $product): ?>
+        <?php foreach ($products as $product) : ?>
             <div class="col-lg-6 mb-4">
-                    <div class="d-flex align-items-center">
-                        <!-- <img class="flex-shrink-0 img-fluid rounded" src="<?= $this->Html->image('menu-' . $product->id . '.jpg', ['alt' => $product->name, 'style' => 'width: 80px;']) ?>" /> -->
-                        <img class="flex-shrink-0 img-fluid rounded" src="img/about-1.jpg" alt="" style="width: 80px;">
+                <div class="d-flex align-items-center">
+                    <!-- <img class="flex-shrink-0 img-fluid rounded" src="<?= $this->Html->image('menu-' . $product->id . '.jpg', ['alt' => $product->name, 'style' => 'width: 80px;']) ?>" /> -->
+                    <img class="flex-shrink-0 img-fluid rounded" src="img/about-1.jpg" alt="" style="width: 80px;">
 
-                        <div class="w-100 d-flex flex-column text-start ps-4">
-                            <h5 class="d-flex justify-content-between border-bottom pb-2">
-                                <span><?= h($product->name) ?></span>
-                                <span class="text-primary">$<?= $this->Number->format($product->price) ?></span>
-                            </h5>
-                            <small class="fst-italic"><?= h($product->description) ?></small>
-                            <div class="d-grid gap-2 mt-3">
-                              </div>
+                    <div class="w-100 d-flex flex-column text-start ps-4">
+                        <h5 class="d-flex justify-content-between border-bottom pb-2">
+
+                            <!-- Use Html->link to make only the product name a link -->
+                            <?= $this->Html->link(
+                                h($product->name), // Product name as link text
+                                ['action' => 'view', $product->id], // Link target (view action with product ID)
+                                ['escape' => false] // Allow HTML content within link
+                            ) ?> 
+                            
+                            <span class="text-primary">$<?= $this->Number->format($product->price) ?></span>
+                        </h5>
+                        <small class="fst-italic"><?= h($product->description) ?></small>
+                        <div class="d-grid gap-2 mt-3">
                         </div>
                     </div>
+                </div>
             </div>
         <?php endforeach; ?>
     </div>
@@ -46,7 +54,7 @@
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div> -->
 </div>
-    <!-- <?= $this->Html->link(__('New Product'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+<!-- <?= $this->Html->link(__('New Product'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Products') ?></h3>
     <div class="table-responsive">
         <table>
@@ -60,7 +68,7 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($products as $product): ?>
+                <?php foreach ($products as $product) : ?>
                 <tr>
                     <td><?= h($product->id) ?></td>
                     <td><?= h($product->name) ?></td>
