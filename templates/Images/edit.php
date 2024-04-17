860dcd3180a6dc7215704f49521673d5972e1a31
Eriks <este0023@student.monash.edu>
2024-04-17 23:38:06 +1000

    Removed image edit page, replaced with "please create a new image instead"

--- templates/Images/edit.php
+++ templates/Images/edit.php
@@ -8,38 +8,5 @@
 <?php if($this->Identity->get('type') != "emp") : ?>
     <div class="alert alert-danger">You do not have privileges to view this page.</div>
 <?php else : ?>
-    <div class="row">
-        <aside class="column">
-            <div class="side-nav">
-                <h4 class="heading"><?= __('Actions') ?></h4>
-                <?= $this->Form->postLink(
-                    __('Delete'),
-                    ['action' => 'delete', $image->id],
-                    ['confirm' => __('Are you sure you want to delete # {0}?', $image->id), 'class' => 'side-nav-item']
-                ) ?>
-                <?= $this->Html->link(__('List Images'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            </div>
-        </aside>
-        <div class="column column-80">
-            <div class="images form content">
-                <tr>
-                <img class="flex-sm-shrink-0 img-fluid rounded" src="<?= $image->file_location ?>" alt="not">
-                </tr>
-                <?= $this->Form->create($image) ?>
-                <fieldset>
-                    <legend><?= __('Edit Image') ?></legend>
-                    <?php
-                    echo $this->Form->control('file_location');
-                    echo $this->Form->control('products._ids', [
-                        'options' => $products,
-                        'empty' => 'Select an Image', 
-                        'label' => 'Choose an Image',
-                         'multiple' => false,]);
-                    ?>
-                </fieldset>
-                <?= $this->Form->button(__('Submit')) ?>
-                <?= $this->Form->end() ?>
-            </div>
-        </div>
-    </div>
+    <div class="alert alert-danger">Please create a new image instead.</div>
 <?php endif; ?>
