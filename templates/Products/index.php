632216e29e766dab7c67c1352abf2ddd1ad82979
Eriks <este0023@student.monash.edu>
2024-04-18 13:23:14 +1000

    Removed customer access to products index page

--- templates/Products/index.php
+++ templates/Products/index.php
@@ -5,6 +5,9 @@
  * @var iterable<\App\Model\Entity\Product> $products
  */
 ?>
+<?php if($this->Identity->get('type') != "emp") : ?>
+    <div class="alert alert-danger">You do not have privileges to view this page.</div>
+<?php else : ?>
 <div class="container-xxl py-5 bg-dark hero-header mb-5">
     <div class="container text-center my-5 pt-5 pb-4">
         <h1 class="display-3 text-white mb-3 animated slideInDown">Items</h1>
@@ -19,48 +22,52 @@
 
 
 
-<div class="container my-4">
-    <div class="d-flex justify-content-end mb-3">
-        <?= $this->Html->link(__('New Product'), ['action' => 'add'], ['class' => 'btn btn-primary']) ?>
-    </div>
+    <div class="container my-4">
+        <div class="d-flex justify-content-end mb-3">
+            <?= $this->Html->link(__('New Product'), ['action' => 'add'], ['class' => 'btn btn-primary']) ?>
+        </div>
 
-    <h3><?= __('Products') ?></h3>
+        <h3><?= __('Products') ?></h3>
 
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                 <tr>
                     <th><?= $this->Paginator->sort('name') ?></th>
                     <th><?= $this->Paginator->sort('price') ?></th>
                     <th><?= $this->Paginator->sort('description') ?></th>
                     <th><?= __('Actions') ?></th>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 <?php foreach ($products as $product): ?>
-                <tr>
-                    <td><?= h($product->name) ?></td>
-                    <td><?= $this->Number->format($product->price) ?> $ </td>
-                    <td><?= h($product->description) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $product->id], ['class' => 'btn btn-sm btn-info']) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $product->id], ['class' => 'btn btn-sm btn-primary']) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $product->id], ['confirm' => __('Are you sure you want to delete # {0}?', $product->id), 'class' => 'btn btn-sm btn-danger']) ?>
-                    </td>
-                </tr>
+                    <tr>
+                        <td><?= h($product->name) ?></td>
+                        <td><?= $this->Number->format($product->price) ?> $ </td>
+                        <td><?= h($product->description) ?></td>
+                        <td class="actions">
+                            <?= $this->Html->link(__('View'), ['action' => 'view', $product->id], ['class' => 'btn btn-sm btn-info']) ?>
+                            <?php if($this->Identity->get('type') == "emp") : ?>
+                                <div class="alert alert-danger">You do not have privileges to view this page.</div>
+                                <?= $this->Html->link(__('Edit'), ['action' => 'edit', $product->id], ['class' => 'btn btn-sm btn-primary']) ?>
+                                <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $product->id], ['confirm' => __('Are you sure you want to delete # {0}?', $product->id), 'class' => 'btn btn-sm btn-danger']) ?>
+                            <?php endif ?>
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
+                </tbody>
+            </table>
+        </div>
 
-    <div class="paginator py-3">
-        <ul class="pagination justify-content-center">
-            <?= $this->Paginator->first('<< ' . __('first'), ['class' => 'page-item']) ?>
-            <?= $this->Paginator->prev('< ' . __('previous'), ['class' => 'page-item']) ?>
-            <?= $this->Paginator->numbers(['class' => 'page-item']) ?>
-            <?= $this->Paginator->next(__('next') . ' >', ['class' => 'page-item']) ?>
-            <?= $this->Paginator->last(__('last') . ' >>', ['class' => 'page-item']) ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+        <div class="paginator py-3">
+            <ul class="pagination justify-content-center">
+                <?= $this->Paginator->first('<< ' . __('first'), ['class' => 'page-item']) ?>
+                <?= $this->Paginator->prev('< ' . __('previous'), ['class' => 'page-item']) ?>
+                <?= $this->Paginator->numbers(['class' => 'page-item']) ?>
+                <?= $this->Paginator->next(__('next') . ' >', ['class' => 'page-item']) ?>
+                <?= $this->Paginator->last(__('last') . ' >>', ['class' => 'page-item']) ?>
+            </ul>
+            <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+        </div>
     </div>
-</div>
+<?php endif; ?>
