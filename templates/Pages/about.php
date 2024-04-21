739305f23b93bebd63e623fa2ec99d4cf482e232
shan0052 <shan0052@student.monash.edu>
2024-04-21 18:31:33 +1000

    add the button on about page

--- templates/Pages/about.php
+++ templates/Pages/about.php
@@ -30,6 +30,7 @@
                         <p class="mb-4">Where passion for homemade food meets convenience and quality. Founded during the COVID-19 pandemic by a group of individuals led by co-founder Apurba, Tasty Bites Kitchen started as a creative response to job losses, transforming shared culinary hobbies into a thriving business venture.</p>
                         <p class="mb-4">At Tasty Bites, we take pride in crafting delicious homemade meals for both individual customers seeking flavorful dishes and wholesale clients looking to cater memorable events. Our diverse menu, featuring rotating set menus posted weekly, reflects our commitment to offering fresh, innovative, and mouthwatering options.</p>
                         <p class="mb-4">Join us on this exciting journey as we continue to serve delectable homemade meals and explore new culinary horizons. Your satisfaction is our priority, and we look forward to sharing our passion for food with you at Tasty Bites Kitchen.</p>
+                        <a class="btn btn-primary py-3 px-5 mt-2 wow fadeInUp" href="<?= $this->Url->build('/Menus') ?>" role="button">Order Now</a>
             <!-- <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a> -->
                 </div>
             </div>
