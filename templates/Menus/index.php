4c0a5d15a86f14086e04566f096d7a0bd8eed0d7
Phuong Anh Chu (pchu0023) <pchu0023@student.monash.edu>
2024-05-02 23:02:30 +1000

    Merge branch 'improve-menu-index-view' into main Modify the format of menu from table to cart layout

--- templates/Menus/index.php
+++ templates/Menus/index.php
@@ -1,64 +1,36 @@
 <?php
-
 /**
  * @var \App\View\AppView $this
  * @var iterable<\App\Model\Entity\Menu> $menus
  */
 ?>
 <div class="menus index content">
-    
-        <!-- display for customer -->
-        <div class="container-xxl py-5 bg-dark hero-header mb-5">
-            <div class="container text-center my-5 pt-5 pb-4">
-                <h1 class="display-3 text-white mb-3 animated slideInDown">Current Active Menu</h1>
-            </div>
+    <!-- display for customer -->
+    <div class="container-xxl py-5 bg-dark hero-header mb-5">
+        <div class="container text-center my-5 pt-5 pb-4">
+            <h1 class="display-3 text-white mb-3 animated slideInDown">Current Active Menu</h1>
         </div>
-
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('description') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- If there is only one menu, just show it -->
-                <?php
-                $active_array = array();
-                foreach ($menus as $menu) :
-                    if ($menu->active === TRUE) :
-                        array_push($active_array, $menu);
-                    endif;
-                endforeach;
-                if (sizeof($active_array) == 1) : ?>
-                    <tr>
-                        <td><?= h($active_array[0]->name) ?></td>
-                        <td><?= h($active_array[0]->description) ?></td>
-                        <td class="actions">
-                            <?= $this->Html->link(__('View'), ['action' => 'view', $active_array[0]->id], ['class' => 'btn btn-secondary py-sm-2 px-sm-3 me-2']) ?>
-                            
-                        </td>
-                    </tr>
-                    <!-- Else, give the option to view each menu -->
-                <?php else : ?>
-                    <?php foreach ($menus as $menu) : ?>
-                        <?php if ($menu->active === TRUE) : ?>
-                            <tr>
-                                <td><?= h($menu->name) ?></td>
-                                <td><?= h($menu->description) ?></td>
-                                <td class="actions">
-                                    <?= $this->Html->link(__('View'), ['action' => 'view', $menu->id], ['class' => 'btn btn-secondary py-sm-2 px-sm-3 me-2']) ?>
-                                   
-                                </td>
-                            </tr>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </tbody>
-        </table>
     </div>
-    <br />
-    
+
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <?php foreach ($menus as $menu) : ?>
+                <?php if ($menu->active === TRUE) : ?>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h3 class="card-title text-center"><?= h($menu->name) ?></h3>
+                                <p class="card-text text-center mb-4"><?= h($menu->description) ?></p>
+                                <div class="text-center">
+                                    <?= $this->Html->link(__('VIEW'), ['action' => 'view', $menu->id], ['class' => 'btn btn-primary py-2 px-3 me-2']) ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <br>
+    <br>
 </div>
