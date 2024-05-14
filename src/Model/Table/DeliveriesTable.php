48c8f7c7c11ebb1828839a8a2507079435b9c53c
shil0010 <shil0010@student.monash.edu>
2024-05-14 17:08:38 +1000

    - Added the beforeFind event to DeliveriesTable.php to intercept all queries to the DeliveriesTable, and ensure they only return Deliveries entities which aren't archived. - Updated DeliveriesController.delete() to  change the product's "isArchived" bool to true instead of deleting the product

--- src/Model/Table/DeliveriesTable.php
+++ src/Model/Table/DeliveriesTable.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
+use Cake\Event\EventInterface;
 use Cake\ORM\Query\SelectQuery;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
+use ArrayObject;
 
 /**
  * Deliveries Model
@@ -67,5 +69,19 @@ class DeliveriesTable extends Table
             ->notEmptyDate('date');
 
         return $validator;
+    }
+
+    /**
+     * @param EventInterface $event
+     * @param SelectQuery $query the query being modified
+     * @param ArrayObject $options
+     * @param $primary
+     * @return SelectQuery the modified query
+     *
+     * Modifies all queries to the Deliveries Table to only return entities not currently Archived
+     */
+    public function beforeFind(EventInterface $event, SelectQuery $query, ArrayObject $options, $primary) {
+        $query->where(['isArchived' => false]);
+        return $query;
     }
 }
